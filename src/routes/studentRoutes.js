4a60051c7762c4c84a6779479cddc87d6d0dbfb0
RozhR <rozhroman@gmail.com>
2026-06-27 21:24:54 +0300

    First commit

---
+++ src/routes/studentRoutes.js
@@ -0,0 +1,13 @@
+import {Router} from "express";
+import {
+    addStudent,
+    findStudent,
+
+} from "../controller/studentController.js";
+
+const router = Router();
+
+router.post('/student', addStudent);
+router.get('/student/:id', findStudent);
+
+export default router;
---
+++ src/service/studentService.js
@@ -0,0 +1,31 @@
+export const addStudent = async (student) => {
+    // TODO: Implement addStudent, Return true if student was successfully and false otherwise
+}
+
+export const findStudent = async (id) => {
+    // TODO: Implement findStudent
+}
+
+export const deleteStudent = async (id) => {
+    // TODO: Implement deleteStudent
+}
+
+export const updateStudent = async (id, data) => {
+    // TODO: Implement updateStudent
+}
+
+export const addScore = async (id, exam, score) => {
+    // TODO: Implement addScore
+}
+
+export const findStudentByName = async (name) => {
+    // TODO: Implement findestudentbyname
+}
+
+export const countStudentByNames = async (names) => {
+    // TODO: Implement findStudentByName
+}
+
+export const findStudentByMinScore = async (exam, minScore) => {
+    // TODO: Implement findStudentByMinScore
+}
