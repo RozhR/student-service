1f117c8737aa2dfad78eb8baddbc9298f3cb3160
RozhR <rozhroman@gmail.com>
2026-06-29 19:11:41 +0300

    Refactor Student repository to use lean() and exclude sensitive fields in queries

--- src/service/studentService.js
+++ src/service/studentService.js
@@ -1,36 +1,18 @@
-import * as repo from '../repository/studentRepository.js';
-import {findStudentById} from "../repository/studentRepository.js";
+import Student from '../model/student.js'
 
-export const addStudent = async ({id, name, password}) => {
-    if (await findStudentById(id)) {
-        return false;
-    }
-    await repo.createStudent({_id: id, name, password});
-    return true;
+export const createStudent = student => Student.create(student);
+
+export const findStudentById = id => Student.findById(id).select({password: 0}).lean().exec();
+
+export const deleteStudent = id => Student.findByIdAndDelete(id).select({password: 0}).lean().exec();
+
+export const updateStudent = (id, data) => Student.findByIdAndUpdate(id, data, {returnDocument: 'after'}).select({scores: 0}).lean().exec();
+
+export const findStudentsByName = name => Student.find({name: new RegExp(`^${name}$`, 'i')}).select({password: 0}).lean().exec();
+
+export const countStudentsByNames = names => {
+    const regexConditions = names.map(name => ({name: {$regex: `^${name}$`, $options: 'i'}}));
+    return Student.countDocuments({$or: regexConditions});
 }
 
-export const findStudent = async (id) => renameId(await repo.findStudentById(+id));
-
-
-export const deleteStudent = async (id) => renameId(await repo.deleteStudent(+id));
-
-export const updateStudent = async (id, data) => renameId(await repo.updateStudent(+id, data));
-
-export const addScore = async (id, exam, score) => repo.updateStudent(+id, {[`scores.${exam}`]: score});
-
-export const findStudentsByName = async (name) => (await repo.findStudentsByName(name)).map(renameId);
-
-export const countStudentsByNames = async (names) => {
-    names = Array.isArray(names) ? names : [names];
-    return repo.countStudentsByNames(names);
-}
-
-export const findStudentsByMinScore = async (exam, minScore) => (await repo.findStudentsByMinScore(exam, +minScore)).map(renameId);
-
-function renameId(student) {
-    if (student) {
-        student.id = student._id;
-        delete student._id;
-    }
-    return student;
-}
+export const findStudentsByMinScore = (exam, minScore) => Student.find({[`scores.${exam}`]: {$gte: minScore}}).select({password: 0}).lean().exec();
